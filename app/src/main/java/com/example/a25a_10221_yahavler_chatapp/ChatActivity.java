c278d0051c596a3e9d66e5c1810b02512af57bb0
yahavLer <118624372+yahavLer@users.noreply.github.com>
2025-01-25 19:52:45 +0200

    start view activity

--- app/src/main/java/com/example/a25a_10221_yahavler_chatapp/ChatActivity.java
+++ app/src/main/java/com/example/a25a_10221_yahavler_chatapp/ChatActivity.java
@@ -1,24 +1,32 @@
 package com.example.a25a_10221_yahavler_chatapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ChatActivity extends AppCompatActivity {
-
+    private RecyclerView recyclerView;
+    private EditText editMessage;
+    private Button btnSend;
+    private MessageAdapter messageAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_chat);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        findViews();
+
+    }
+
+    private void findViews() {
+        recyclerView = findViewById(R.id.recycler_chat_list);
+        editMessage = findViewById(R.id.edit_message);
+        btnSend = findViewById(R.id.btn_send);
     }
 }
