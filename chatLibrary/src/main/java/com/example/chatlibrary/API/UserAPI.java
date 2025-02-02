33ea5ef9bb210679f6104dd43804d0dac6ac2ecc
yahavLer <118624372+yahavLer@users.noreply.github.com>
2025-02-02 13:29:02 +0200

    update

--- chatLibrary/src/main/java/com/example/chatlibrary/API/UserAPI.java
+++ chatLibrary/src/main/java/com/example/chatlibrary/API/UserAPI.java
@@ -18,9 +18,11 @@ public interface UserAPI {
     @GET("api/users/userId/{userId}")
     Call<User> getById(@Path("userId") String userId);
 
+    @GET("api/users/all")
+    Call<List<User>> getAllUsers(@Query("size") int size, @Query("page") int page);
+
     @GET("api/users/username/{username}/phoneNumber/{phoneNumber}")
     Call<User> getByUsernameAndPhone(@Path("username") String username, @Path("phoneNumber")String phoneNumber);
 
-    @GET("api/users/all")
-    Call<List<User>> getAllUsers(@Query("size") int size, @Query("page") int page);
+
 }
